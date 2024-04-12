08cc63f995d80d555bc8021fc800eb4369baba9b
renanNobrega <renan.pereira@anjunexpress.com.br>
2024-04-12 11:24:30 -0300

    Fix/FixNewCommit2

---
+++ santander-dev-week-2024/src/main/java/me/dio/sdw24/adapters/out/ChampionsJdbcRepository.java
@@ -0,0 +1,37 @@
+package me.dio.sdw24.adapters.out;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import me.dio.sdw24.domain.model.Champions;
+import me.dio.sdw24.domain.ports.ChampionsRepository;
+
+@Repository
+public class ChampionsJdbcRepository implements ChampionsRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Champions> rowMapper;
+
+    public ChampionsJdbcRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.rowMapper = ((rs, rowNum) -> new Champions(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getString("role"),
+            rs.getString("lane")
+        ))
+    }
+
+    @Override
+    public List<Champions> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<Champions> findById(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+}
